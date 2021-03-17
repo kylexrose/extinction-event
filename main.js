ac1005d59bc303301b4baa8fa70672d619dd7873
kylexrose@gmail.com <kylexrose@gmail.com>
2021-03-17 15:55:16 -0400

    accomplished all stretch goals

--- main.js
+++ main.js
@@ -1,33 +1,40 @@
-const list1 = document.querySelectorAll('ol li');
-for (let item of list1){
-    item.addEventListener('click', (e) =>{
-        e.target.style.textDecoration = "line-through";
-    })
-}
-
-const list2 = document.querySelectorAll('ul li');
-for (let item of list2){
-    item.addEventListener('click', (e) =>{
-        e.target.style.opacity = "0";
-    })
-}
-
-const list3 = document.querySelectorAll('#row img');
-for (let item of list3){
-    item.addEventListener('click', (e) =>{
-        e.target.style.width = "0";
-    })
-}
-
+// Meteor Button ===========================
 const button = document.querySelector('button');
 button.addEventListener('click', () =>{
-    for (let item of list1){
-        item.style.textDecoration = "line-through";
-    }
-    for (let item of list2){
-        item.style.opacity = "0";
-    }
-    for (let item of list3){
-        item.style.width = "0";
-    }
+    list1.forEach((item) => strike(item))
+    list2.forEach((item) => disappear(item))
+    list3.forEach((item) => collapse(item))
 })
+
+//Individual Item Clicks==========================
+const list1 = document.querySelectorAll('ol li');
+list1.forEach((item) => {
+    item.addEventListener('click', (e) => {
+        strike(e.target);
+})})
+
+const list2 = document.querySelectorAll('ul li');
+list2.forEach((item) => {
+    item.addEventListener('click', (e) => {
+    disappear(e.target);
+})})
+
+const list3 = document.querySelectorAll('img');
+list3.forEach((item) => {
+    item.addEventListener('click', (e) => {
+    collapse(e.target);
+})})
+
+//Helper Functions ==========================
+
+function strike(element){
+    element.style.textDecoration = "line-through";
+}
+
+function disappear(element){
+    element.style.opacity = "0";
+}
+
+function collapse(element){
+    element.style.width = "0";
+}
